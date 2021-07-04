52fdff9775666da11cb69777e98d89b7f0965878
soymd <pacificstate33@gmail.com>
2021-07-04 12:30:40 +0900

    abc207b

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -14,81 +14,84 @@ class MainKtTest {
     fun test_hydrate() {
         val (a, b, c, d) = listOf(5, 2, 3, 2)
 
-        val actual = hydrate(a, b, c, d)
-
-        assertThat(actual, equalTo(2))
+        assertThat(hydrate(a, b, c, d), equalTo(2))
     }
 
     @Test
     fun test_hydrate_2() {
         val (a, b, c, d) = listOf(6, 9, 2, 3)
 
-        val actual = hydrate(a, b, c, d)
+        assertThat(hydrate(a, b, c, d), equalTo(-1))
+    }
 
-        assertThat(actual, equalTo(-1))
+    @Test
+    fun test_hydrate_3() {
+        val (a, b, c, d) = listOf(2, 1, 1, 2)
+
+        assertThat(hydrate(a, b, c, d), equalTo(2))
+    }
+
+    @Test
+    fun test_hydrate_4() {
+        val (a, b, c, d) = listOf(10, 1, 2, 2)
+
+        assertThat(hydrate(a, b, c, d), equalTo(4))
     }
 
     @Test
     fun test_hydrate_5() {
         val (a, b, c, d) = listOf(1, 1, 1, 1)
 
-        val actual = hydrate(a, b, c, d)
-
-        assertThat(actual, equalTo(-1))
+        assertThat(hydrate(a, b, c, d), equalTo(-1))
     }
 
     @Test
     fun test_hydrate_6() {
         val (a, b, c, d) = listOf(1, 2, 2, 2)
 
-        val actual = hydrate(a, b, c, d)
-
-        assertThat(actual, equalTo(1))
+        assertThat(hydrate(a, b, c, d), equalTo(1))
     }
 
     @Test
     fun test_hydrate_7() {
         val (a, b, c, d) = listOf(2, 2, 2, 2)
 
-        val actual = hydrate(a, b, c, d)
-
-        assertThat(actual, equalTo(1))
+        assertThat(hydrate(a, b, c, d), equalTo(1))
     }
 
     @Test
-    fun test_hydrate_4() {
-        val a = 55038
-        val b = 30598
-        val c = 48487
-        val d = 18961
-        val actual = hydrate(a, b, c, d)
+    fun test_hydrate_8() {
+        val (a, b, c, d) = listOf(2, 2, 2, 1)
 
-        assertThat(actual, equalTo(1))
+        assertThat(hydrate(a, b, c, d), equalTo(-1))
     }
 
-//    @Test
-//    fun test_hydrate_3() {
-//        val num = 1..100000
-//        var (a, b, c, d) = listOf(0, 0, 0, 0)
-//        var actual: Int
-//        var n = 0
-//        do {
-//            a = num.random()
-//            b = num.random()
-//            c = num.random()
-//            d = num.random()
-//            actual = hydrate(a, b, c, d)
-//            n += 1
-//            println("foooooo")
-//            println(n)
-//            println(a)
-//            println(b)
-//            println(c)
-//            println(d)
-//        } while (n <= 1000000)
-//
-//        println(n)
-//
-//        assertThat(actual, equalTo(-1))
-//    }
+    @Test
+    fun test_hydrate_9() {
+        val (a, b, c, d) = listOf(1, 2, 2, 1)
+
+        assertThat(hydrate(a, b, c, d), equalTo(-1))
+    }
+
+    @Test
+    fun test_hydrate_10() {
+        val (a, b, c, d) = listOf(1, 2, 2, 1)
+
+        assertThat(hydrate(a, b, c, d), equalTo(-1))
+    }
+
+    @Test
+    fun test_hydrate_11() {
+        val (a, b, c, d) = listOf(1, 1, 1, 2)
+
+        assertThat(hydrate(a, b, c, d), equalTo(1))
+    }
+
+    @Test
+    fun test_hydrate_12() {
+        val (a, b, c, d) = listOf(99792, 61343, 5112, 12)
+
+        assertThat(hydrate(a, b, c, d), equalTo(99792))
+    }
+
 }
