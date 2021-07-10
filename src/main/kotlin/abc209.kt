ff73f9ef1e8d5b3dbb725bef07260b2a0bf6dc20
soymd <pacificstate33@gmail.com>
2021-07-10 21:23:18 +0900

    abc209b

--- src/main/kotlin/abc209.kt
+++ src/main/kotlin/abc209.kt
@@ -1,9 +1,26 @@
 fun main(args: Array<String>) {
     val (n, k) = readLine()!!.split(" ").map { it.toInt() }
-    val answer = counting(n, k)
+    val list = readLine()!!.split(" ").map { it.toInt() }
+    val answer = canYouBuyThemAll(n, k, list)
 
     println(answer)
 //    println(answer.joinToString(separator = System.lineSeparator()))
+}
+
+fun canYouBuyThemAll(count: Int, money: Int, product: List<Int>): String {
+    var temp = 0
+    product.forEachIndexed { index, i ->
+        if ((index + 1) % 2 == 0) {
+            temp += i - 1
+        } else {
+            temp += i
+        }
+    }
+    return if ((money - temp) >= 0) {
+        "Yes"
+    } else {
+        "No"
+    }
 }
 
 fun counting(a: Int, b: Int): Int {
--- src/test/kotlin/ABC209Test.kt
+++ src/test/kotlin/ABC209Test.kt
@@ -29,4 +29,34 @@ class ABC209Test {
     fun counting_2_2() {
         assertThat(counting(2, 2), equalTo(0))
     }
+
+    @Test
+    fun `Can you buy them all 2 3 1 3`() {
+        assertThat(canYouBuyThemAll(2, 3, listOf(1, 3)), equalTo("Yes"))
+    }
+
+    @Test
+    fun `Can you buy them all 4 10 3 3 4 4`() {
+        assertThat(canYouBuyThemAll(4, 10, listOf(3, 3, 4, 4)), equalTo("No"))
+    }
+
+    @Test
+    fun `Can you buy them all 4 10 3 3 4 2`() {
+        assertThat(canYouBuyThemAll(3, 10, listOf(3, 3, 4)), equalTo("Yes"))
+    }
+
+    @Test
+    fun `Can you buy them all 4 10 3 3`() {
+        assertThat(canYouBuyThemAll(3, 10, listOf(3, 3, 5)), equalTo("Yes"))
+    }
+
+    @Test
+    fun `Can you buy them all 3 10`() {
+        assertThat(canYouBuyThemAll(3, 10, listOf(3, 3, 6)), equalTo("No"))
+    }
+
+    @Test
+    fun `Can you buy them all 8 30 `() {
+        assertThat(canYouBuyThemAll(8, 30, listOf(3, 1, 4, 1, 5, 9, 2, 6)), equalTo("Yes"))
+    }
 }
