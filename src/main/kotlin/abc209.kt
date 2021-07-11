5f05c14d5a1551de8fbfd464dcc78e4a225850aa
soymd <pacificstate33@gmail.com>
2021-07-11 14:28:02 +0900

    abc209

--- src/main/kotlin/abc209.kt
+++ src/main/kotlin/abc209.kt
@@ -1,10 +1,58 @@
+@file:Suppress("LocalVariableName", "UnnecessaryVariable")
+
 fun main(args: Array<String>) {
-    val (n, k) = readLine()!!.split(" ").map { it.toInt() }
-    val list = readLine()!!.split(" ").map { it.toInt() }
-    val answer = canYouBuyThemAll(n, k, list)
+    val (n) = readLine()!!.split(" ").map { it.toInt() }
+    val list = readLine()!!.split(" ").map { it.toLong() }
+    val answer = notEqual(n, list)
 
     println(answer)
 //    println(answer.joinToString(separator = System.lineSeparator()))
+}
+
+//10720中5267
+fun notEqual(n: Int, c: List<Long>): Long {
+    if (n != c.count()) throw Exception("invalid test condition!")
+    /*
+    1 ≤ Ai ≤ Ci (1 ≤ i ≤ N)
+    Ai ≠ Aj     (1 ≤ i < j ≤ N)
+    cの中の最大値 = n c.count = r
+    総数 = nPr
+    */
+    var answer = 1L
+    c.sorted().forEachIndexed { index, l ->
+        val max = l - index//max(0, l - index)
+        answer = answer * max % 1000000007
+    }
+    return answer
+//    val max = c.maxBy { it }!!
+//    val totalPermutation = permutation(max, n)
+//
+//    var diffPattern = 0L
+//    c.forEachIndexed { index, i ->
+//        val maxMinusI = max - i
+//        val minusPerDiff = factorial(maxMinusI)
+//        val diff = maxMinusI * minusPerDiff
+//        diffPattern += diff
+//    }
+//    val answer = totalPermutation - diffPattern
+//    return answer
+}
+
+fun permutation(n: Long, r: Int): Long {
+    var answer = 1L
+    val l = n - r + 1
+    for (i in l..n) {
+        answer *= i
+    }
+    return answer
+}
+
+fun factorial(n: Long): Long {
+    var answer = 1L
+    for (i in 1..n) {
+        answer *= i
+    }
+    return answer
 }
 
 fun canYouBuyThemAll(count: Int, money: Int, product: List<Int>): String {
--- src/test/kotlin/ABC209Test.kt
+++ src/test/kotlin/ABC209Test.kt
@@ -59,4 +59,263 @@ class ABC209Test {
     fun `Can you buy them all 8 30 `() {
         assertThat(canYouBuyThemAll(8, 30, listOf(3, 1, 4, 1, 5, 9, 2, 6)), equalTo("Yes"))
     }
+
+    @Test
+    fun `notEqual 2, 1 3`() {
+        assertThat(notEqual(2, listOf(1, 3)), equalTo(2L))
+        /*
+          // 1 2
+             1 3
+          // 2 1
+             2 3
+          // 3 1
+          // 3 2
+        */
+    }
+
+    @Test
+    fun `notEqual 2, 1 1`() {
+        assertThat(notEqual(2, listOf(1, 1)), equalTo(0L))
+        /*
+        totalPermutation = 1 になってそこから何も引かれない(diffPattern = 0)ままテスト終了
+         - totalPermutationの出し方が不味い
+         - diffPatternの出し方が不味い
+
+        */
+    }
+
+    @Test
+    fun `notEqual 4, 3 3 4 4`() {
+        assertThat(notEqual(4, listOf(3, 3, 4, 4)), equalTo(12L))
+        /*
+        i:1 j:2
+        i:1 j:3
+        i:1 j:4
+        i:2 j:3
+        i:2 j:4
+        i:3 j:4
+
+        1 2 3 4
+        1 2 4 3
+        1 3 2 4
+        1 3 4 2
+    //        1 4 2 3
+    //        1 4 3 2
+
+        2 1 3 4
+        2 1 4 3
+        2 3 1 4
+        2 3 4 1
+    //        2 4 1 3
+    //        2 4 3 1
+
+        3 2 1 4
+        3 2 4 1
+        3 1 2 4
+        3 1 4 2
+    //        3 4 2 1
+    //        3 4 1 2
+
+    //        4 2 3 1
+    //        4 2 1 3
+    //        4 3 2 1
+    //        4 3 1 2
+    //        4 1 2 3
+    //        4 1 3 2
+        */
+    }
+
+    @Test
+    fun `notEqual 4, 1 3 4 4`() {
+        assertThat(notEqual(4, listOf(1, 3, 4, 4)), equalTo(4L))
+        /*
+        1 2 3 4
+        1 2 4 3
+        1 3 2 4
+        1 3 4 2
+    //        1 4 2 3
+    //        1 4 3 2
+
+//        2 1 3 4
+//        2 1 4 3
+//        2 3 1 4
+//        2 3 4 1
+    //        2 4 1 3
+    //        2 4 3 1
+
+//        3 2 1 4
+//        3 2 4 1
+//        3 1 2 4
+//        3 1 4 2
+    //        3 4 2 1
+    //        3 4 1 2
+
+    //        4 2 3 1
+    //        4 2 1 3
+    //        4 3 2 1
+    //        4 3 1 2
+    //        4 1 2 3
+    //        4 1 3 2
+        */
+    }
+
+    @Test
+    fun `notEqual 4, 1 4 4 4`() {
+        assertThat(notEqual(4, listOf(1, 4, 4, 4)), equalTo(6L))
+        /*
+        1 2 3 4
+        1 2 4 3
+        1 3 2 4
+        1 3 4 2
+        1 4 2 3
+        1 4 3 2
+
+//        2 1 3 4
+//        2 1 4 3
+//        2 3 1 4
+//        2 3 4 1
+    //        2 4 1 3
+    //        2 4 3 1
+
+//        3 2 1 4
+//        3 2 4 1
+//        3 1 2 4
+//        3 1 4 2
+    //        3 4 2 1
+    //        3 4 1 2
+
+    //        4 2 3 1
+    //        4 2 1 3
+    //        4 3 2 1
+    //        4 3 1 2
+    //        4 1 2 3
+    //        4 1 3 2
+        */
+    }
+
+    @Test
+    fun `notEqual 4, 2 3 4 4`() {
+        assertThat(notEqual(4, listOf(2, 3, 4, 4)), equalTo(8L))
+        /*
+        1 2 3 4
+        1 2 4 3
+        1 3 2 4
+        1 3 4 2
+    //        1 4 2 3
+    //        1 4 3 2
+
+        2 1 3 4
+        2 1 4 3
+        2 3 1 4
+        2 3 4 1
+    //        2 4 1 3
+    //        2 4 3 1
+
+//        3 2 1 4
+//        3 2 4 1
+//        3 1 2 4
+//        3 1 4 2
+    //        3 4 2 1
+    //        3 4 1 2
+
+    //        4 2 3 1
+    //        4 2 1 3
+    //        4 3 2 1
+    //        4 3 1 2
+    //        4 1 2 3
+    //        4 1 3 2
+        */
+    }
+
+    @Test
+    fun `notEqual 3, 1 3 4`() {
+        assertThat(notEqual(3, listOf(1, 3, 4)), equalTo(4L))
+
+        /*
+        1 2 3
+        1 2 4
+        1 3 2
+        1 3 4
+//        1 4 2
+//        1 4 3
+
+        2 1 3
+        2 1 4
+        2 3 1
+        2 3 4
+        2 4 1
+        2 4 3
+
+        3 2 1
+        3 2 4
+        3 1 2
+        3 1 4
+        3 4 2
+        3 4 1
+
+        4 2 3
+        4 2 1
+        4 3 2
+        4 3 1
+        4 1 2
+        4 1 3
+        */
+    }
+
+    @Test
+    fun `notEqual 10,`() {
+        assertThat(
+            notEqual(
+                10,
+                listOf(
+                    999999917,
+                    999999914,
+                    999999923,
+                    999999985,
+                    999999907,
+                    999999965,
+                    999999914,
+                    999999908,
+                    999999951,
+                    999999979
+                )
+            ), equalTo(405924645L)
+        )
+    }
+
+
+    @Test
+    fun `permutation 5P5`() {
+        assertThat(permutation(5, 5), equalTo(120L))
+    }
+
+    @Test
+    fun `permutation 5P4`() {
+        assertThat(permutation(5, 4), equalTo(120L))
+    }
+
+    @Test
+    fun `permutation 5P3`() {
+        assertThat(permutation(5, 3), equalTo(60L))
+    }
+
+    @Test
+    fun `permutation 5P2`() {
+        assertThat(permutation(5, 2), equalTo(20L))
+    }
+
+    @Test
+    fun `permutation 5P1`() {
+        assertThat(permutation(5, 1), equalTo(5L))
+    }
+
+    @Test
+    fun `factorial 5`() {
+        assertThat(factorial(5), equalTo(120L))
+    }
+
+    @Test
+    fun `factorial 1`() {
+        assertThat(factorial(1), equalTo(1L))
+    }
 }
