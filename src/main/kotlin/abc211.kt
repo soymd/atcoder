4ac133d0e95b8feb2aad17ccc2bd181ed59338d0
soymd <pacificstate33@gmail.com>
2021-07-25 10:19:21 +0900

    wip abc211 C

--- src/main/kotlin/abc211.kt
+++ src/main/kotlin/abc211.kt
@@ -1,10 +1,47 @@
 fun main() {
-    val (s1) = readLine()!!.split(" ").map { it.toString() }
-    val (s2) = readLine()!!.split(" ").map { it.toString() }
-    val (s3) = readLine()!!.split(" ").map { it.toString() }
-    val (s4) = readLine()!!.split(" ").map { it.toString() }
+    val (s) = readLine()!!.split(" ").map { it.toString() }
 //    val list = readLine()!!.split(" ").map { it.toLong() }
-    println(cycleHit(s1, s2, s3, s4))
+    println(chokudai(s))
+}
+
+fun chokudai(s: String): Int {
+    if (
+        !s.contains("c")
+        || !s.contains("h")
+        || !s.contains("o")
+        || !s.contains("k")
+        || !s.contains("u")
+        || !s.contains("d")
+        || !s.contains("a")
+        || !s.contains("i")
+    ) {
+        return 0
+    }
+    var lastIndexContainsChokudai: Int
+    var cPosition = s.indexOf("c")
+    do {
+        var bool: Boolean
+        if (cPosition == -1) {
+            bool = false
+        } else {
+            val subStr = s.substring(cPosition)
+            bool = subStr.contains("h")
+                && subStr.contains("o")
+                && subStr.contains("k")
+                && subStr.contains("u")
+                && subStr.contains("d")
+                && subStr.contains("a")
+                && subStr.contains("i")
+            if (bool) {
+                lastIndexContainsChokudai = cPosition
+                cPosition = s.indexOf("c", startIndex = lastIndexContainsChokudai + 1)
+            }
+        }
+    } while (bool)
+
+    var answer = 0
+
+    return answer / 1000000007
 }
 
 fun cycleHit(s1: String, s2: String, s3: String, s4: String): String {
--- src/test/kotlin/ABC211Test.kt
+++ src/test/kotlin/ABC211Test.kt
@@ -11,6 +11,36 @@ class ABC211Test {
     }
 
     @Test
+    fun chokudai_chchokudai() {
+        assertThat(chokudai("chchokudai"), equalTo(3))
+    }
+
+    @Test
+    fun chokudai_ccchokudai() {
+        assertThat(chokudai("ccchokudai"), equalTo(3))
+    }
+
+    @Test
+    fun chokudai_chchokudaic() {
+        assertThat(chokudai("chchokudaic"), equalTo(3))
+    }
+
+    @Test
+    fun chokudai_atcoderrr() {
+        assertThat(chokudai("atcoderrr"), equalTo(0))
+    }
+
+    @Test
+    fun chokudai_chokudaichokudaichokudai() {
+        assertThat(chokudai("chokudaichokudaichokudai"), equalTo(45))
+    }
+
+    @Test
+    fun chokudai_chokudaichokudai() {
+        assertThat(chokudai("chokudaichokudai"), equalTo(9))
+    }
+
+    @Test
     fun cycleHit_1() {
         assertThat(
             cycleHit("3B", "HR", "2B", "H"),
@@ -34,13 +64,13 @@ class ABC211Test {
         )
     }
 
-    @Test
-    fun bloodPressure_300_50() {
-        assertThat(
-            bloodPressure(300, 50),
-            equalTo(133.3333333F)
-        )
-    }
+//    @Test
+//    fun bloodPressure_300_50() {
+//        assertThat(
+//            bloodPressure(300, 50),
+//            equalTo(133.3333333F)
+//        )
+//    }
 
     @Test
     fun bloodPressure_123_123() {
