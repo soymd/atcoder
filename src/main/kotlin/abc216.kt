8a8b6e7a8d4c14b3f58f5126f428f1abd79b2c6f
soymd <pacificstate33@gmail.com>
2021-08-29 21:05:11 +0900

    abc216 a

--- src/main/kotlin/abc216.kt
+++ src/main/kotlin/abc216.kt
@@ -1,2 +1,15 @@
 fun main() {
+    signedDifficulty()
+}
+
+fun signedDifficulty() {
+    val (x, y) = readLine()!!.split(".").map(String::toInt)
+    val str = when (y) {
+        in 0..2 -> "-"
+        in 3..6 -> ""
+        else -> "+"
+    }
+
+    print(x)
+    println(str)
 }
