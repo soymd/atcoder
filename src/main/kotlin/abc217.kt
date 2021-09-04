2c9d0f59b9e35a4d9b01491505c22bcba6e95e8d
soymd <pacificstate33@gmail.com>
2021-09-04 21:14:46 +0900

    abc217 b

--- src/main/kotlin/abc217.kt
+++ src/main/kotlin/abc217.kt
@@ -1,5 +1,18 @@
 fun main() {
-    lexicographicOrder()
+    atCoderQuiz()
+}
+
+fun atCoderQuiz() {
+    val s1 = readLine()!!
+    val s2 = readLine()!!
+    val s3 = readLine()!!
+    val list = mutableListOf("ABC", "ARC", "AGC", "AHC")
+
+    list.remove(s1)
+    list.remove(s2)
+    list.remove(s3)
+
+    println(list.first())
 }
 
 fun lexicographicOrder() {
