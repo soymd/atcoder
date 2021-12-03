e7110cea4c860cdaed67f4999fb4c6e4760f4023
soymd <pacificstate33@gmail.com>
2021-12-03 21:27:00 +0900

    abc230 b ac

--- src/main/kotlin/abc230.kt
+++ src/main/kotlin/abc230.kt
@@ -1,5 +1,38 @@
 fun main() {
-    atCoderQuiz3()
+    tripleMetre()
+}
+
+fun tripleMetre() {
+    val s = readLine()!!.trim().split("").filter { it != "" }.toMutableList()
+
+    if (s == mutableListOf("x") || s == mutableListOf("x", "x") || s == mutableListOf("o")) {
+        println("Yes")
+        return
+    }
+
+    val firstIndex = s.indexOf("o")
+
+    if (firstIndex > 2 || firstIndex == -1) {
+        println("No")
+        return
+    }
+    val sublist = s.subList(firstIndex, s.count())
+
+    sublist.forEachIndexed { index, string ->
+        if (index % 3 == 0) {
+            if (string == "x") {
+                println("No")
+                return
+            }
+        } else {
+            if (string == "o") {
+                println("No")
+                return
+            }
+        }
+    }
+
+    println("Yes")
 }
 
 fun atCoderQuiz3() {
--- src/test/kotlin/ABC230Test.kt
+++ src/test/kotlin/ABC230Test.kt
@@ -7,6 +7,94 @@ import org.junit.jupiter.api.Test
 
 class ABC230Test {
     @Nested
+    inner class TripleMetre {
+        private val input = StandardInputStream()
+        private val output = StandardOutputStream()
+
+        @BeforeEach
+        fun setUp() {
+            System.setIn(input)
+            System.setOut(output)
+        }
+
+        @AfterEach
+        fun tearDown() {
+            System.setIn(null)
+            System.setOut(null)
+        }
+
+        @Test
+        fun tripleMetre_xoxxoxxo() {
+            input.inputln("xoxxoxxo")
+
+            tripleMetre()
+
+            val result = output.readLines()
+            assertThat(result, equalTo(listOf("Yes")))
+        }
+
+        @Test
+        fun tripleMetre_xxoxxoxo() {
+            input.inputln("xxoxxoxo")
+
+            tripleMetre()
+
+            val result = output.readLines()
+            assertThat(result, equalTo(listOf("No")))
+        }
+
+        @Test
+        fun tripleMetre_o() {
+            input.inputln("o")
+
+            tripleMetre()
+
+            val result = output.readLines()
+            assertThat(result, equalTo(listOf("Yes")))
+        }
+
+        @Test
+        fun tripleMetre_ox() {
+            input.inputln("ox")
+
+            tripleMetre()
+
+            val result = output.readLines()
+            assertThat(result, equalTo(listOf("Yes")))
+        }
+
+        @Test
+        fun tripleMetre_x() {
+            input.inputln("x")
+
+            tripleMetre()
+
+            val result = output.readLines()
+            assertThat(result, equalTo(listOf("Yes")))
+        }
+
+        @Test
+        fun tripleMetre_oo() {
+            input.inputln("oo")
+
+            tripleMetre()
+
+            val result = output.readLines()
+            assertThat(result, equalTo(listOf("No")))
+        }
+
+        @Test
+        fun tripleMetre_xxxxxxxx() {
+            input.inputln("xxxxxxxx")
+
+            tripleMetre()
+
+            val result = output.readLines()
+            assertThat(result, equalTo(listOf("No")))
+        }
+    }
+
+    @Nested
     inner class AtCoderQuiz3 {
         private val input = StandardInputStream()
         private val output = StandardOutputStream()
