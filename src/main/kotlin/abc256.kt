acaff94faefd9d6a5db4fc2d071dab0882a6c46d
soymd <pacificstate33@gmail.com>
2022-06-29 20:19:38 +0900

    abc256 b

--- src/main/kotlin/abc256.kt
+++ src/main/kotlin/abc256.kt
@@ -1,7 +1,68 @@
 import kotlin.math.pow
 
 fun main() {
-    twoN()
+    batters()
+}
+
+fun batters() {
+    val (n) = readLine()!!.trim().split(" ").map { it.toInt() }
+    val a = readLine()!!.trim().split(" ").map { HitFactory.create(it.toInt()) }
+
+    var score = 0
+    var runners = listOf(0, 0, 0)
+    a.forEach { batter ->
+        val result = batter.hit(runners)
+        runners = result.first
+        score += result.second
+    }
+
+    println(score)
+}
+
+interface Hit {
+    fun hit(runners: List<Int>): Pair<List<Int>, Int>
+}
+
+class Single : Hit {
+    override fun hit(runners: List<Int>): Pair<List<Int>, Int> {
+        val score = runners[2]
+        val third = runners[1]
+        val second = runners[0]
+        return Pair(listOf(1, second, third), score)
+    }
+}
+
+class TwoBase : Hit {
+    override fun hit(runners: List<Int>): Pair<List<Int>, Int> {
+        val score = runners[1] + runners[2]
+        val third = runners[0]
+        return Pair(listOf(0, 1, third), score)
+    }
+}
+
+class ThreeBase : Hit {
+    override fun hit(runners: List<Int>): Pair<List<Int>, Int> {
+        return Pair(listOf(0, 0, 1), runners.sum())
+    }
+}
+
+class HomeRun : Hit {
+    override fun hit(runners: List<Int>): Pair<List<Int>, Int> {
+        return Pair(listOf(0, 0, 0), runners.sum() + 1)
+    }
+}
+
+class HitFactory {
+    companion object {
+        fun create(n: Int): Hit {
+            return when (n) {
+                1 -> Single()
+                2 -> TwoBase()
+                3 -> ThreeBase()
+                else -> HomeRun()
+            }
+        }
+    }
 }
 
 fun twoN() {
--- src/test/kotlin/ABC256Test.kt
+++ src/test/kotlin/ABC256Test.kt
@@ -63,22 +63,46 @@ class ABC256Test {
 
         @Test
         fun a_1() {
-            input.inputln("")
+            input.inputln("4")
+            input.inputln("1 1 3 2")
 
-            //()
+            batters()
 
             val result = output.readLines()
-            assertThat(result, equalTo(listOf("")))
+            assertThat(result, equalTo(listOf("3")))
         }
 
         @Test
         fun a_2() {
-            input.inputln("")
+            input.inputln("3")
+            input.inputln("1 1 1")
 
-            //()
+            batters()
 
             val result = output.readLines()
-            assertThat(result, equalTo(listOf("")))
+            assertThat(result, equalTo(listOf("0")))
+        }
+
+        @Test
+        fun a_3() {
+            input.inputln("10")
+            input.inputln("2 2 4 1 1 1 4 2 2 1")
+
+            batters()
+
+            val result = output.readLines()
+            assertThat(result, equalTo(listOf("8")))
+        }
+
+        @Test
+        fun a_4() {
+            input.inputln("10")
+            input.inputln("2 2 4 1 1 1 4")
+
+            batters()
+
+            val result = output.readLines()
+            assertThat(result, equalTo(listOf("7")))
         }
     }
 
