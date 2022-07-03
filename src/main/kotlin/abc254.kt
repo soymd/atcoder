c8029aac0589481ebd02dcc91881192ea354a788
soymd <pacificstate33@gmail.com>
2022-07-03 13:37:01 +0900

    abc254 b

--- src/main/kotlin/abc254.kt
+++ src/main/kotlin/abc254.kt
@@ -1,5 +1,32 @@
 fun main() {
-    lastTwoDigits()
+    practicalComputing()
+}
+
+
+fun practicalComputing() {
+    val n = readLine()!!.trim().toInt()
+    val ans = mutableListOf<MutableList<Int>>()
+    repeat(n) { i ->
+        val list = mutableListOf<Int>()
+        repeat(i + 1) { j ->
+            val isZero = j == 0
+            val isLast = j == i
+            if (isZero || isLast) {
+                list.add(1)
+            } else {
+                val temp = ans[i - 1]
+                val i1 = temp.getOrNull(j - 1) ?: 0
+                val i2 = temp.getOrNull(j) ?: 0
+                if (i1 != 0 && i2 != 0) {
+                    list.add(i1 + i2)
+                }
+            }
+        }
+        ans.add(list)
+    }
+    ans.forEach { a ->
+        println(a.joinToString(" "))
+    }
 }
 
 fun lastTwoDigits() {
--- src/test/kotlin/ABC254Test.kt
+++ src/test/kotlin/ABC254Test.kt
@@ -83,33 +83,117 @@ class ABC254Test {
 
         @Test
         fun a_1() {
-            input.inputln("")
+            input.inputln("1")
 
-            //()
-
-            val result = output.readLines()
-            assertThat(result, equalTo(listOf("")))
-        }
-
-        @Test
-        fun a_2() {
-            input.inputln("")
-
-            //()
+            practicalComputing()
 
             val result = output.readLines()
-            assertThat(result, equalTo(listOf("")))
+            assertThat(result, equalTo(listOf("1")))
         }
 
         @Test
         fun a_3() {
-            input.inputln("")
+            input.inputln("3")
 
-            //()
+            practicalComputing()
 
             val result = output.readLines()
-            assertThat(result, equalTo(listOf("")))
+            assertThat(
+                result, equalTo(
+                    listOf(
+                        "1",
+                        "1 1",
+                        "1 2 1"
+                    )
+                )
+            )
         }
+
+        @Test
+        fun a_4() {
+            input.inputln("4")
+
+            practicalComputing()
+
+            val result = output.readLines()
+            assertThat(
+                result, equalTo(
+                    listOf(
+                        "1",
+                        "1 1",
+                        "1 2 1",
+                        "1 3 3 1"
+                    )
+                )
+            )
+        }
+
+        @Test
+        fun a_5() {
+            input.inputln("5")
+
+            practicalComputing()
+
+            val result = output.readLines()
+            assertThat(
+                result, equalTo(
+                    listOf(
+                        "1",
+                        "1 1",
+                        "1 2 1",
+                        "1 3 3 1",
+                        "1 4 6 4 1"
+                    )
+                )
+            )
+        }
+
+        @Test
+        fun a_6() {
+            input.inputln("6")
+
+            practicalComputing()
+
+            val result = output.readLines()
+            assertThat(
+                result, equalTo(
+                    listOf(
+                        "1",
+                        "1 1",
+                        "1 2 1",
+                        "1 3 3 1",
+                        "1 4 6 4 1",
+                        "1 5 10 10 5 1"
+                    )
+                )
+            )
+        }
+
+        @Test
+        fun a_10() {
+            input.inputln("10")
+
+            practicalComputing()
+
+            val result = output.readLines()
+            assertThat(
+                result, equalTo(
+                    listOf(
+                        "1",
+                        "1 1",
+                        "1 2 1",
+                        "1 3 3 1",
+                        "1 4 6 4 1",
+                        "1 5 10 10 5 1",
+                        "1 6 15 20 15 6 1",
+                        "1 7 21 35 35 21 7 1",
+                        "1 8 28 56 70 56 28 8 1",
+                        "1 9 36 84 126 126 84 36 9 1"
+                    )
+                )
+            )
+        }
+
 
 //        @Test
 //        fun a_4() {
